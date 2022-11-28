d6d43f30bb0a0e0a2772a64c86a0ad6a5eb4ba7f
Amit Patra <amitbaranpatra@gmail.com>
2022-11-28 09:59:45 +0530

    Commit my changes

--- data_dump.py
+++ data_dump.py
@@ -5,19 +5,20 @@ import json
 # Provide the mongodb localhost url to connect python to mongodb.
 client = pymongo.MongoClient("mongodb://localhost:27017/neurolabDB")
 
-DATA_FILE_PATH = "https://raw.githubusercontent.com/avnyadav/sensor-fault-detection/main/aps_failure_training_set1.csv"
-DATABASE_NAME= "aps"
-COLLECTION_NAME= "sensor"
+DATA_FILE_PATH="/config/workspace/aps_failure_training_set1.csv"
+DATABASE_NAME="aps"
+COLLECTION_NAME="sensor"
+
 
 if __name__=="__main__":
-    df=pd.read_csv(DATA_FILE_PATH)
+    df = pd.read_csv(DATA_FILE_PATH)
     print(f"Rows and columns: {df.shape}")
 
-    #Convert dataframe to json so that we can dump thesr records into mongodb
+    #Convert dataframe to json so that we can dump these record in mongo db
     df.reset_index(drop=True,inplace=True)
 
-    json_records = list(json.loads(df.T.to_json()).values())
-    print(json_records[0])
+    json_record = list(json.loads(df.T.to_json()).values())
+    print(json_record[0])
 
-    #insert converted json records to mongodb
-    client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_records)
+    #insert converted json record to mongo db
+    client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_record)
---
+++ sensor/__init__.py
@@ -0,0 +1,3 @@
+from dotenv import load_dotenv
+print(f"Loading environment vartaible from .env file")
+load_dotenv()
